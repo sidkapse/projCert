5756922b3aa4e85681bb9d4860c6485421272f70
siddharth <sid.kapse@gmail.com>
2021-05-26 14:10:59 +0800

    removed unwanted files

--- dockerbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-pwd
-cd /home/ansible_user/job1
-docker build . -t siddharthkapse/edurekaproject-php
-docker run -d -p 8081:80 siddharthkapse/edurekaproject-php
--- runSeleniumShell.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar /tmp/selenium_test/Selenium_PHP_Test.jar
